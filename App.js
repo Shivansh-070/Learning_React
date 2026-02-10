c08cefdd1348db14631d7269ce5dece43d1acf45
Shivansh-070 <125335466+Shivansh-070@users.noreply.github.com>
2026-02-10 23:00:07 +0530

    dynamic data along with data destructuring

--- App.js
+++ App.js
@@ -4,6 +4,729 @@ import ReactDOM from "react-dom/client"//latest
 
 // const heading = React.createElement("h1",{},"I am h1 from App.js");
 
+const resList = [
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "722374",
+"name": "Pizza Hut",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/f0e97f3a-c96a-413f-8c37-6e58d6572c0a_722374.JPG",
+"locality": "Nehru Nagar",
+"areaName": "Nehru Nagar",
+"costForTwo": "₹350 for two",
+"cuisines": [
+"Pizzas"
+],
+"avgRating": 4.2,
+"parentId": "721",
+"avgRatingString": "4.2",
+"totalRatingsString": "1.5K+",
+"sla": {
+"deliveryTime": 47,
+"lastMileTravel": 3,
+"serviceability": "SERVICEABLE",
+"slaString": "45-50 mins",
+"lastMileTravelString": "3.0 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-02-10 23:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹59"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "4.5",
+"ratingCount": "549"
+},
+"source": "GOOGLE",
+"sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-2e462f34-13b5-4272-a116-9543c57de143"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/noida/pizza-hut-nehru-nagar-rest722374",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "390466",
+"name": "Wow! Momo",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/30/10b1b3c6-3d6f-4aef-aa95-773f80fbb711_390466.jpg",
+"locality": "Nehru Nagar",
+"areaName": "Nehru Nagar",
+"costForTwo": "₹300 for two",
+"cuisines": [
+"Momos",
+"Chinese",
+"fastfood",
+"Asian",
+"Beverages"
+],
+"avgRating": 4.3,
+"parentId": "1776",
+"avgRatingString": "4.3",
+"totalRatingsString": "2.2K+",
+"sla": {
+"deliveryTime": 39,
+"lastMileTravel": 3,
+"serviceability": "SERVICEABLE",
+"slaString": "35-40 mins",
+"lastMileTravelString": "3.0 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-02-11 01:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "Ratnesh_Badges/Rx_Awards_2025/Momo.png",
+"description": "Delivery!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "Delivery!",
+"imageId": "Ratnesh_Badges/Rx_Awards_2025/Momo.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹69"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "4.7",
+"ratingCount": "439"
+},
+"source": "GOOGLE",
+"sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-2e462f34-13b5-4272-a116-9543c57de143"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/noida/wow-momo-nehru-nagar-rest390466",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "302707",
+"name": "Barbeque Nation",
+"cloudinaryImageId": "sihzgcpcxszv7fzvv0ri",
+"locality": "Raj Nagar",
+"areaName": "Raj Nagar",
+"costForTwo": "₹600 for two",
+"cuisines": [
+"North Indian",
+"Barbecue",
+"Kebabs",
+"Biryani",
+"Street Food",
+"Snacks"
+],
+"avgRating": 4.1,
+"parentId": "2438",
+"avgRatingString": "4.1",
+"totalRatingsString": "1.6K+",
+"sla": {
+"deliveryTime": 45,
+"lastMileTravel": 4.4,
+"serviceability": "SERVICEABLE",
+"slaString": "40-45 mins",
+"lastMileTravelString": "4.4 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-02-10 23:30:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹74"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "5.0",
+"ratingCount": "<3"
+},
+"source": "GOOGLE",
+"sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-2e462f34-13b5-4272-a116-9543c57de143"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/noida/barbeque-nation-raj-nagar-rest302707",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "659251",
+"name": "Prasuma Momo Kitchen",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/19/fe66f24d-b35c-43de-b4a9-efb882644c95_659251.jpg",
+"locality": "Vasundhara",
+"areaName": "Vasundhara",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Momos",
+"Chinese",
+"Asian",
+"Japanese",
+"Tibetan",
+"Street Food",
+"Beverages"
+],
+"avgRating": 4.5,
+"parentId": "283975",
+"avgRatingString": "4.5",
+"totalRatingsString": "1.0K+",
+"sla": {
+"deliveryTime": 42,
+"lastMileTravel": 7.8,
+"serviceability": "SERVICEABLE",
+"slaString": "40-45 mins",
+"lastMileTravelString": "7.8 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-02-11 03:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹119"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-2e462f34-13b5-4272-a116-9543c57de143"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/noida/prasuma-momo-kitchen-vasundhara-rest659251",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "1076668",
+"name": "Cruffin Premium - Cakes and Desserts",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/12/1c11ed11-d442-41b8-9b5c-c615fa329ca6_1076668.JPG",
+"locality": "Raj Nagar",
+"areaName": "Raj Nagar",
+"costForTwo": "₹250 for two",
+"cuisines": [
+"Bakery",
+"Desserts"
+],
+"avgRating": 4.6,
+"veg": true,
+"parentId": "436110",
+"avgRatingString": "4.6",
+"totalRatingsString": "546",
+"sla": {
+"deliveryTime": 33,
+"lastMileTravel": 5,
+"serviceability": "SERVICEABLE",
+"slaString": "30-35 mins",
+"lastMileTravelString": "5.0 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-02-11 01:00:00",
+"opened": true
+},
+"badges": {},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "60% OFF",
+"subHeader": "UPTO ₹120"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-2e462f34-13b5-4272-a116-9543c57de143"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/noida/cruffin-premium-cakes-and-desserts-raj-nagar-rest1076668",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "643811",
+"name": "WeFit - Protein Bowls, Salads & Sandwiches",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/2/3/a8725196-0f10-491d-96a0-74b5b6a113a3_643811.jpg",
+"locality": "RDC",
+"areaName": "Raj Nagar",
+"costForTwo": "₹250 for two",
+"cuisines": [
+"Healthy Food",
+"Salads",
+"Keto",
+"Snacks"
+],
+"avgRating": 4.8,
+"parentId": "355285",
+"avgRatingString": "4.8",
+"totalRatingsString": "1.1K+",
+"sla": {
+"deliveryTime": 25,
+"lastMileTravel": 4.6,
+"serviceability": "SERVICEABLE",
+"slaString": "20-30 mins",
+"lastMileTravelString": "4.6 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-02-11 02:00:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "High%20Protein/rx%20tag%205.png",
+"description": "High Protein"
+},
+{
+"imageId": "Ratnesh_Badges/Rx_Awards_2025/Healthy.png",
+"description": "Delivery!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "High Protein",
+"imageId": "High%20Protein/rx%20tag%205.png"
+}
+},
+{
+"attributes": {
+"description": "Delivery!",
+"imageId": "Ratnesh_Badges/Rx_Awards_2025/Healthy.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹99"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-2e462f34-13b5-4272-a116-9543c57de143"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/noida/wefit-protein-bowls-salads-and-sandwiches-rdc-raj-nagar-rest643811",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "1023576",
+"name": "Veer Ji Malai Chaap Wale",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/10/8ade7c5d-62cf-4379-a620-053ea502d720_1023576.jpg",
+"locality": "Raj Nagar",
+"areaName": "Raj Nagar",
+"costForTwo": "₹400 for two",
+"cuisines": [
+"Chinese",
+"Street Food",
+"Snacks",
+"Fast Food"
+],
+"avgRating": 4.2,
+"parentId": "12056",
+"avgRatingString": "4.2",
+"totalRatingsString": "507",
+"sla": {
+"deliveryTime": 29,
+"lastMileTravel": 1.7,
+"serviceability": "SERVICEABLE",
+"slaString": "25-30 mins",
+"lastMileTravelString": "1.7 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-02-10 23:45:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "android/static-assets/icons/big_rx.png",
+"description": "bolt!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "bolt!",
+"imageId": "android/static-assets/icons/big_rx.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹149"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-2e462f34-13b5-4272-a116-9543c57de143"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/noida/veer-ji-malai-chaap-wale-raj-nagar-rest1023576",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+"@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+"info": {
+"id": "999597",
+"name": "Thalaiva Biryani",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/9/9/d7c9b5ff-f393-4e35-bf39-8bda6d5fd58b_999597.JPG",
+"locality": "Azad Building",
+"areaName": "Navyug Market",
+"costForTwo": "₹250 for two",
+"cuisines": [
+"Biryani",
+"Mughlai",
+"Lucknowi",
+"Hyderabadi",
+"Kebabs",
+"Desserts",
+"Beverages"
+],
+"avgRating": 4.4,
+"parentId": "582792",
+"avgRatingString": "4.4",
+"totalRatingsString": "397",
+"sla": {
+"deliveryTime": 40,
+"lastMileTravel": 2.2,
+"serviceability": "SERVICEABLE",
+"slaString": "35-40 mins",
+"lastMileTravelString": "2.2 km",
+"iconType": "ICON_TYPE_EMPTY"
+},
+"availability": {
+"nextCloseTime": "2026-02-10 23:59:00",
+"opened": true
+},
+"badges": {
+"imageBadges": [
+{
+"imageId": "android/static-assets/icons/big_rx.png",
+"description": "bolt!"
+}
+]
+},
+"isOpen": true,
+"type": "F",
+"badgesV2": {
+"entityBadges": {
+"imageBased": {
+"badgeObject": [
+{
+"attributes": {
+"description": "bolt!",
+"imageId": "android/static-assets/icons/big_rx.png"
+}
+}
+]
+},
+"textBased": {},
+"textExtendedBadges": {}
+}
+},
+"aggregatedDiscountInfoV3": {
+"header": "ITEMS",
+"subHeader": "AT ₹89"
+},
+"orderabilityCommunication": {
+"title": {},
+"subTitle": {},
+"message": {},
+"customIcon": {}
+},
+"differentiatedUi": {
+"displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+"differentiatedUiMediaDetails": {
+"mediaType": "ADS_MEDIA_ENUM_IMAGE",
+"lottie": {},
+"video": {}
+}
+},
+"reviewsSummary": {},
+"displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+"restaurantOfferPresentationInfo": {},
+"externalRatings": {
+"aggregatedRating": {
+"rating": "--"
+}
+},
+"ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+},
+"analytics": {
+"context": "seo-data-2e462f34-13b5-4272-a116-9543c57de143"
+},
+"cta": {
+"link": "https://www.swiggy.com/city/noida/thalaiva-biryani-azad-building-navyug-market-rest999597",
+"text": "RESTAURANT_MENU",
+"type": "WEBLINK"
+},
+"widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+}
+]
+
 const Heading = () =>{
   return (
     <div className="header">
@@ -23,17 +746,19 @@ const Heading = () =>{
   );
 };
 
-const RestaurantCard =()=>{
+const RestaurantCard =(props)=>{
+  const {resData} = props;
 return(
   <div className="res-card"> 
      <img className="res-logo" 
      alt="rest-logo"
-      src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/9/1/f0e97f3a-c96a-413f-8c37-6e58d6572c0a_722374.JPG"
+      src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+resData.info.cloudinaryImageId}
      />
-     <h3>Meghana Foods</h3>
-     <h4>Biryani, North Indian</h4>
-     <h4>Ratings 4.3</h4>
-     <h4>38 mins</h4>
+     <h3>{resData.info.name}</h3>
+     <h4 className="cuisines">{resData.info.cuisines.join(", ") }</h4>
+     <h4>{resData.info.avgRating}</h4>
+     <h4>{resData.info.costForTwo}</h4>
+     <h4>{resData.info.sla.deliveryTime}</h4>
   </div>
  
 );
@@ -44,21 +769,16 @@ const Body =()=>{
 return(
   <div className="body">
     <div className="search"></div>
-     {/* Restaurant card */}
      <div className="res-container">
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
-        <RestaurantCard/>
+        <RestaurantCard resData = {resList[0]}/>   {/* resData is key */}
+        <RestaurantCard resData = {resList[1]}/>
+        <RestaurantCard resData = {resList[2]}/>
+        <RestaurantCard resData = {resList[3]}/>
+        <RestaurantCard resData = {resList[4]}/>
+        <RestaurantCard resData = {resList[5]}/>
+        <RestaurantCard resData = {resList[6]}/>
+        <RestaurantCard resData = {resList[7]}/>
+        {/* <RestaurantCard resName = "KFC" cuisine = "Burger" /> */}
      </div>
   
 
--- README.md
+++ README.md
@@ -13,7 +13,17 @@
     -Consistent Hashing
     -Code splitting
     -Differential bundling -support older version
+    -Error handling
+    -HTTPS
+    -Tree shaking algo - remove unused code
+    -Different build for dev and and production
 
 # package.json and package-lock.json files for dependencies installation
 # remove main : App.js
 # Add node_modules/.parcel-cache/dist into git ignore file
+
+
+# Props to pass data to component we do it using prosps or are just arg to function
+
+# config driven UI
+   -a design pattern where the user interface structure, layout, and behavior are defined by external configuration files (usually JSON or YAML) rather than hard-coded
